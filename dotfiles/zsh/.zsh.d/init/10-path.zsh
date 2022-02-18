a80cd68871e802d39963d62369ea9b953c446ad6
ez2take <gbiifvrc517@gmail.com>
2022-02-18 20:24:04 +0900

    rm path.zsh

--- dotfiles/zsh/.zsh.d/init/10-path.zsh
+++
@@ -1,2 +0,0 @@
-export PATH=~/.local/bin:/snap/bin:/usr/sandbox/:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:$PATH
-export PATH=~/tools/dependency-check/bin:$PATH
