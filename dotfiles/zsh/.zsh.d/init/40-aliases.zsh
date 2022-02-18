007845179fed85fa3266c4c54b6c1cd475fbbd1b
ez2take <gbiifvrc517@gmail.com>
2022-02-18 20:07:08 +0900

    K

--- dotfiles/zsh/.zsh.d/init/40-aliases.zsh
+++ dotfiles/zsh/.zsh.d/init/40-aliases.zsh
@@ -2,6 +2,9 @@ alias ls="ls --color=auto"
 alias l="ls"
 alias ll="ls -lAF"
 alias c="cd"
+alias chx="chmod a+x"
+alias gdb="gdb -q"
+alias t="tmux"
 
 if $nvim ; then
     alias n="nvim"
@@ -9,3 +12,4 @@ fi
 
 alias nvimconfig="nvim ~/.config/nvim"
 alias zshconfig="nvim ~/.zsh.d/init; source ~/.zshrc"
+alias tmuxconfig="nvim ~/.tmux.conf; tmux source-file ~/.tmux.conf"
