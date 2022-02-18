b57da993fa57cade080a62884aefbd86f6ea2cd2
kui8shi <gbiifvrc517@gmail.com>
2022-02-18 19:01:31 +0900

    zsh completion ignorering case

--- dotfiles/zsh/.zsh.d/init/20-env.zsh
+++ dotfiles/zsh/.zsh.d/init/20-env.zsh
@@ -3,4 +3,4 @@ export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
 export SHELL=`which zsh`
-
+export GHIDRA_INSTALL_DIR="${HOME}/ghidra"
