2688c35a81914b8fe015d273c03f99b1fb5fa8db
kui8shi <gbiifvrc517@gmail.com>
2024-07-31 19:58:42 +0900

    aa

--- components/xkb/xkbsetup.sh
+++ components/xkb/xkbsetup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-test -f $HOME/.xkb/keymap/mykbd && \
+test -f $HOME/.xkb/keymap/mykbd && which xkbcomp >/dev/null && \
   xkbcomp -I$HOME/.xkb $HOME/.xkb/keymap/mykbd $DISPLAY 2> /dev/null
-
 # When this doesn't work in your terminal sessions but X11 applications,
 # xkb may be overwritten by other input engines. In my case, it was wayland.
 # I disabled wayland editing /etc/gdm3/custom.conf to uncomment the line of WaylandEnable=false.
