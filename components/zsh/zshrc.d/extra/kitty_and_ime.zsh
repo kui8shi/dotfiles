f80cd21d85ea97e63c92d404e1652a09c708768f
kui8shi <gbiifvrc517@gmail.com>
2025-05-03 17:44:21 +0900

    Update dotfiles according to desktop environment renewal (X11 -> Wayland)

--- components/zsh/zshrc.d/extra/kitty_and_ime.zsh
+++ components/zsh/zshrc.d/extra/kitty_and_ime.zsh
@@ -1,7 +1,7 @@
 if type kitty >/dev/null 2>&1; then
-  # for japanese input
-  export GLFW_IM_MODULE="ibus kitty"
+  # This variable need to be set to "ibus" no matter what the IME you are using (?)
+  # c.f. https://fcitx-im.org/wiki/Setup_Fcitx_5#Other_less_common_setup
+  export GLFW_IM_MODULE="ibus"
   # alias for clipboard function
   alias clip="kitty +kitten clipboard"
 fi
-export XMODIFIERS=@im=fcitx
